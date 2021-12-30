03ec608784b2845744ed965d8a9cf014d540d3ae
Tal <tal.delarosa@gmail.com>
2021-12-29 23:38:25 -0800

    Initial commit of DrawingHelper

---
+++ DrawingHelper/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+
+namespace DrawingHelper
+{
+    public class Canvas
+    {
+    }
+
+    public class Header
+    {
+    }
+
+    public class Body
+    {
+    }
+
+    public class Footer
+    {
+    }
+
+    public class Section
+    {
+
+    }
+
+    public class Menu
+    {
+    }
+}
